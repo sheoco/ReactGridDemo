f2f14f834e511a55a25d9825a2f92c257b944c5f
Sheo Shankar <sheointime@gmail.com>
2020-07-21 15:40:08 +0530

    React Grid Draggable and Resizable Demo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar.js';
+import Grid from './Grid.js';
+
+const App = ()=> {
+  return (
+    <>
+      <Navbar/>
+      <Grid />
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
+import About from './About';
+import Contact from './Contact';
+
+
+const routing = (  
+  <Router>  
+    <div>  
+     <Route exact path="/" component={App} />  
+      <Route path="/about" component={About} />  
+      <Route path="/contact" component={Contact} />  
+    </div>  
+  </Router>  
+) 
+
+ReactDOM.render(routing, document.getElementById('root'));  
+/*
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+*/
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
